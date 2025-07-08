a96b4ffb05725824d68b5100805abd7915c91c56
00frank <00frankgarcia@gmail>
2025-07-08 01:32:01 -0300

    adapting keyboard to screen size

--- src/screen/setManager/components/SetInput.component.tsx
+++ src/screen/setManager/components/SetInput.component.tsx
@@ -1,4 +1,3 @@
-import SafeInputKeyboard from "@/components/SafeInputKeyboard.component";
 import { ChevronRight } from "lucide-react-native";
 import { useState } from "react";
 import { TextInput, View } from "react-native";
@@ -8,17 +7,15 @@ export default function SetInput() {
     const [value, setValue] = useState("")
 
     return (
-        <SafeInputKeyboard>
-            <View className="border mx-6 px-6 rounded-[16px] h-[64px] border-primary-100 items-center flex-row">
-                <TextInput
-                    onChangeText={setValue}
-                    value={value}
-                    placeholderTextColor={"#443976"}
-                    placeholder="¿De qué tópico quieres hablar?"
-                    className="flex-1 text-md"
-                />
-                <ChevronRight color={"#443976"} />
-            </View>
-        </SafeInputKeyboard>
+        <View className="border mx-6 px-6 rounded-[16px] h-[64px] bg-white border-primary-100 items-center flex-row">
+            <TextInput
+                onChangeText={setValue}
+                value={value}
+                placeholderTextColor={"#443976"}
+                placeholder="¿De qué tópico quieres hablar?"
+                className="flex-1 text-md h-[64px]"
+            />
+            <ChevronRight color={"#443976"} />
+        </View>
     )
 }
--- src/screen/setManager/index.tsx
+++ src/screen/setManager/index.tsx
@@ -1,19 +1,26 @@
 import BackButton from "@/components/BackHeader.component"
-import { View } from "react-native"
-import { useSafeAreaInsets } from "react-native-safe-area-context"
+import { Keyboard, KeyboardAvoidingView, Platform, ScrollView, TouchableWithoutFeedback } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
 import SetManagerBackgroundImage from "./components/SetBackgroundImage.component"
 import SetInput from "./components/SetInput.component"
 
 
 
 export default function SetManagerScreen() {
-    const insets = useSafeAreaInsets();
 
     return (
-        <View className="gap-8" style={{ marginTop: insets.top }}>
+        <SafeAreaView className="flex-1">
             <BackButton />
-            <SetManagerBackgroundImage />
-            <SetInput />
-        </View>
+            <KeyboardAvoidingView
+                className="flex-1"
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <ScrollView className="flex-1">
+                        <SetManagerBackgroundImage />
+                        <SetInput />
+                    </ScrollView>
+                </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
     )
 }
